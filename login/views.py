7cc975032be4dc043846dc76b3ed36bb1ce36f49
mahesh2247 <maheshm.mca18@bmsce.ac.in>
2024-01-30 14:27:18 +0530

    added post routes for login and register

--- login/views.py
+++ login/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
 class Index(View):
@@ -19,8 +21,31 @@ class Login(View):
     def get(self, request):
         return render(request, "login.html")
     
+    def post(self, request):
+        uname = request.POST.get("username", "")
+        passwd = request.POST.get("password", "")
+        user = authenticate(username=uname, password=passwd)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged in")
+            return redirect("index")
+        else:
+            messages.warning(request, "Username or password is incorrect")
+        return render(request, "login.html")
+
+
+@method_decorator(login_required, name="dispatch")   
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, "Signed out!")
+        return redirect("index")
+    
 class Register(View):
     def get(self, request):
+        if request.user.is_authenticated:
+            messages.info(request, "You are already logged in")
+            return redirect("index")
         return render(request, "register.html")
     
     def post(self, request):
