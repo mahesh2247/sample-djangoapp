14fcd9ef5d420c83a9caddada7faf70f44023cdc
mahesh2247 <maheshm.mca18@bmsce.ac.in>
2024-01-30 13:35:47 +0530

    added post method for registering user

--- login/views.py
+++ login/views.py
@@ -1,6 +1,9 @@
 from django.views import View
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 
 class Index(View):
@@ -19,4 +22,19 @@ class Login(View):
 class Register(View):
     def get(self, request):
         return render(request, "register.html")
+    
+    def post(self, request):
+        uname = request.POST.get("username", "")
+        passwd = request.POST.get("password", "")
+        user = User.objects.filter(username=uname).first()
+        if user is None:
+            user = User(username=uname)
+            user.set_password(passwd)
+            user.save()
+            login(request, user)
+            messages.success(request, "User created")
+            return redirect("index")
+        else:
+            messages.info(request, "User already exists")
+            return redirect("register")
 
